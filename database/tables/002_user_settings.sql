45186120b5f26bd605f709669035405a3972e1c4
FarhanHussain <meetfarhan.h@gmail.com>
2026-04-05 02:14:44 -0500

    deleting all to restart

--- database/tables/002_user_settings.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE user_settings (
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    user_id INT UNSIGNED NOT NULL,
-    setting_key VARCHAR(100) NOT NULL,
-    setting_value TEXT NOT NULL,
-    UNIQUE KEY uq_user_setting (user_id, setting_key),
-    CONSTRAINT fk_user_settings_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-) ENGINE=InnoDB;
